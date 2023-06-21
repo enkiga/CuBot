9e73c3287ea5f89cad39d2f69ae6159e4444d6e5
enkiga <eenkiga@gmail.com>
2023-06-22 02:09:54 +0300

    commit

---
+++ events_json_generator.py
@@ -0,0 +1,79 @@
+import mysql.connector
+import json
+
+# connect to database
+mydb = mysql.connector.connect(
+    host="localhost",
+    port="3306",
+    user="root",
+    password="K1pk0r1r!",
+    database="cueabot"
+)
+
+mycursor = mydb.cursor()
+
+existing_data = {'intents': []}
+# load existing JSON data from the file if it exists
+try:
+    with open('event.json', 'r') as json_file:
+        existing_data = json.load(json_file)
+except FileNotFoundError:
+    pass
+
+# Fetch data from table, event in the database
+mycursor.execute("SELECT * FROM events")
+myresult = mycursor.fetchall()
+
+# Create a list to store or update the data
+updated_data = {'intents': []}
+
+# Loop through the data fetched from the database
+for row in myresult:
+    # create parameters for the data
+    event_id = row[0]
+    event_name = row[1]
+    event_venue = row[2]
+    event_date = row[3]
+    event_time = row[4]
+    event_description = row[5]
+
+    # create a dictionary to store the data
+    event_dict = {
+        'tag': f'{event_name}',
+        'patterns': [
+            f'When is {event_name}?',
+            f'What event is happening on {event_date}?',
+            f'At what time is {event_name}?',
+            f'Where is {event_name}?',
+            f'What is {event_name} about?',
+            f'What is happening on {event_date}?',
+            f'What is happening on {event_date} at {event_time}?',
+            f'Where is {event_name} happening?',
+        ],
+        'responses': [
+            f'{event_name} is happening on {event_date} at {event_time} at {event_venue}. {event_description}'],
+        'context_set': ''
+    }
+
+    # check if the data already exists in the JSON file
+    existing_entry = next(
+        (entry for entry in existing_data['intents'] if entry.get('patterns') == event_dict['patterns']),
+        None)
+    if existing_entry:
+        # update the data if it already exists
+        existing_entry.update(event_dict)
+        updated_data['intents'].append(existing_entry)
+    else:
+        # add the data if it doesn't exist
+        updated_data['intents'].append(event_dict)
+
+# Convert the data to JSON format and write it to the file
+json_data = json.dumps(updated_data, indent=4)
+
+# save the data to the file
+with open('event.json', 'w') as json_file:
+    json_file.write(json_data)
+
+# close the database connection
+mycursor.close()
+mydb.close()
